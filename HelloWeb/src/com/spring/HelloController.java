7e3b0aac4dc35670c5f46983873953bd1c31e3a2
Swati <ssucharita3@gmail.com>
2016-11-05 19:24:57 +0530

    commit

---
+++ HelloWeb/src/com/spring/HelloController.java
@@ -0,0 +1,19 @@
+package com.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+public class HelloController{
+ 
+	@RequestMapping("/hello")
+
+   public String printHello(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+
+      return "hello";
+   }
+
+}
